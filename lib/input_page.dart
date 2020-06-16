f4e4ee40f4808aeea41fa575b8f77653f479db3a
Erik Maide <maide.erik@gmail.com>
2020-06-16 23:46:45 +0300

    added SliderTheme data for Slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -88,16 +88,26 @@ class _InputPageState extends State<InputPage> {
                           )
                         ],
                       ),
-                      Slider(
-                        value: height.toDouble(),
-                        min: 42.0,
-                        max: 240.0,
-                        activeColor: Color(0xFFEB1555),
-                        onChanged: (double newValue) {
-                          setState(() {
-                            height = newValue.round();
-                          });
-                        },
+                      SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.white,
+                            inactiveTrackColor: Color(0xFF8D8E98),
+                            thumbColor: Color(0xFFEB1555),
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                            overlayColor: Color(0x29EB1555),
+                            overlayShape:
+                                RoundSliderOverlayShape(overlayRadius: 30.0)),
+                        child: Slider(
+                          value: height.toDouble(),
+                          min: 42.0,
+                          max: 240.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          },
+                        ),
                       )
                     ],
                   ))),
